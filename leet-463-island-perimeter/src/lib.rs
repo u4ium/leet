e396ed4a39a5ad5bd28f7f90a564a49f3e1aa84b
u4ium <joe@armitage.com>
2021-10-04 13:37:41 -0400

    Simpler solution for 463-island-perimeter

--- leet-463-island-perimeter/src/lib.rs
+++ leet-463-island-perimeter/src/lib.rs
@@ -1,83 +1,5 @@
 struct Solution {}
 
-type Grid = Vec<Vec<i32>>;
-
-struct Island {
-    grid: Grid,
-}
-
-impl IntoIterator for Island {
-    type Item = IslandCell;
-    type IntoIter = IslandIterator;
-    fn into_iter(self) -> IslandIterator {
-        IslandIterator {
-            grid: self.grid,
-            row_index: 0,
-            column_index: 0,
-        }
-    }
-}
-
-struct IslandIterator {
-    grid: Grid,
-    row_index: usize,
-    column_index: usize,
-}
-
-struct IslandCell {
-    coastline_length: i32,
-}
-
-impl IslandCell {
-    fn new(grid: &Grid, row_index: usize, column_index: usize) -> Self {
-        let max_row_index = grid.len() - 1;
-        let max_column_index = grid[0].len() - 1;
-        let mut coastline_length = 4;
-        if row_index > 0 {
-            coastline_length -= grid[row_index - 1][column_index];
-        }
-        if row_index < max_row_index {
-            coastline_length -= grid[row_index + 1][column_index];
-        }
-        if column_index > 0 {
-            coastline_length -= grid[row_index][column_index - 1];
-        }
-        if column_index < max_column_index {
-            coastline_length -= grid[row_index][column_index + 1];
-        }
-        IslandCell { coastline_length }
-    }
-
-    fn get_coastline_length(&self) -> i32 {
-        self.coastline_length
-    }
-}
-
-impl Iterator for IslandIterator {
-    type Item = IslandCell;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let max_row_index = self.grid.len() - 1;
-        let max_column_index = self.grid[0].len() - 1;
-        loop {
-            if self.column_index > max_column_index {
-                if self.row_index == max_row_index {
-                    return None;
-                }
-                self.row_index += 1;
-                self.column_index = 0;
-            }
-            if self.grid[self.row_index][self.column_index] == 1 {
-                break;
-            }
-            self.column_index += 1;
-        }
-        let ret = IslandCell::new(&self.grid, self.row_index, self.column_index);
-        self.column_index += 1;
-        Some(ret)
-    }
-}
-
 /// Determine the perimeter of the island represented by the given row x col grid where:
 ///     - grid[i][j] = 1 represents land and
 ///     - grid[i][j] = 0 represents water.
@@ -88,10 +10,36 @@ impl Iterator for IslandIterator {
 ///     - The grid is rectangular, width and height don't exceed 100.
 impl Solution {
     pub fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
-        Island { grid }
-            .into_iter()
-            .map(|island| island.get_coastline_length())
-            .sum()
+        let max_row_index = grid.len() - 1;
+        let max_column_index = grid[0].len() - 1;
+        let mut result = 0;
+        for row_index in 0..=max_row_index {
+            for column_index in 0..=max_column_index {
+                result += match grid[row_index][column_index] {
+                    1 => {
+                        4 - (if row_index > 0 {
+                            grid[row_index - 1][column_index]
+                        } else {
+                            0
+                        } + if row_index < max_row_index {
+                            grid[row_index + 1][column_index]
+                        } else {
+                            0
+                        } + if column_index > 0 {
+                            grid[row_index][column_index - 1]
+                        } else {
+                            0
+                        } + if column_index < max_column_index {
+                            grid[row_index][column_index + 1]
+                        } else {
+                            0
+                        })
+                    }
+                    _ => 0,
+                };
+            }
+        }
+        result
     }
 }
 
